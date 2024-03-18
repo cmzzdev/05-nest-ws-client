f9b68291efbb05f458b6c94ed970f862a856072c
Carles <carmar85@gmail.com>
2024-03-18 16:39:19 +0100

    initial commit

---
+++ src/socket-client.ts
@@ -0,0 +1,68 @@
+import { Manager, Socket } from 'socket.io-client';
+
+let socket: Socket;
+
+export const connectToServer = (token: string) => {
+    // http://localhost:3000/socket.io/socket.io.js
+    const manager: Manager = new Manager('http://localhost:3000/socket.io/socket.io.js', {
+        extraHeaders: {
+            hola: 'mundo',
+            authentication: token
+        }
+    });
+    socket?.removeAllListeners();
+    // connect to namespace (server room). By default is root '/'
+    socket = manager.socket('/');
+    // console.log({socket})
+    addListeners()
+}
+
+const addListeners = () => {
+
+    const serverStatusListener = document.querySelector('#server-status')!;
+    const clientsUl = document.querySelector('#clients-ul')!;
+    const messagesUl = document.querySelector<HTMLUListElement>('#messages-ul')!;
+    const messageForm = document.querySelector<HTMLFormElement>('#message-form')!;
+    const messageInput = document.querySelector<HTMLInputElement>('#message-input')!;
+    
+
+    socket.on('connect', () => {
+        serverStatusListener.innerHTML = 'connected!';
+    })
+    socket.on('disconnect', () => {
+        serverStatusListener.innerHTML = 'disconnected!';
+    })
+    socket.on('clients-updated', (clients: string[]) => {
+        let clientsHtml = '';
+        clients.forEach(clientId => {
+            clientsHtml += `
+                <li>${clientId}</li>
+            `
+        })
+        clientsUl.innerHTML = clientsHtml;
+    });
+
+    messageForm?.addEventListener('submit', (event) => {
+        event.preventDefault();
+        if(messageInput.value.trim().length <= 0) return;
+        
+        socket.emit('message-from-client', {
+            id: 'YO!', 
+            message: messageInput?.value
+        })
+
+        messageInput.value = '';
+    })
+
+    socket.on('message-from-server', (payload: { fullName: string, message: string }) => {
+        const htmlOut = `
+            <li>
+                <strong>${payload.fullName}</strong>
+                <span>${payload.message}</span>
+            </li>
+        `;
+        const li = document.createElement('li');
+        li.innerHTML = htmlOut;
+        messagesUl.append(li)
+    })
+}
